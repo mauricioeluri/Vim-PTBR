5dcb612dce4ddaf1515d6d7ed03795da983b8372
Maurício El Uri <mauriciom.eluri@gmail.com>
2018-02-26 09:44:35 -0300

    Instala o VIM, dependencias e plugins + YouCompleteMe

--- install.sh
+++ install.sh
@@ -1,9 +1,17 @@
 #!/bin/bash
 
+sudo apt-get install vim-nox cmake python-dev
 
 cp -r .vim ~/
 cp .vimrc ~/
 
+git clone https://gist.github.com/8248094.git .vim/colors/monokai
+mv .vim/colors/monokai/Monokai.vim .vim/colors/monokai.vim
+rm -rf .vim/colors/monokai
+
 git clone https://github.com/VundleVim/Vundle.vim.git ~/.vim/bundle/Vundle.vim
 
 vim +PluginInstall +qall
+
+python ~/.vim/bundle/YouCompleteMe/install.py
+
