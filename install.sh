7330be5ade384b162e0affd35052d74c83663cf7
Maurício El Uri <mauriciom.eluri@gmail.com>
2019-02-17 17:09:42 -0300

    Corrigido problema de dependências

--- install.sh
+++ install.sh
@@ -1,23 +1,31 @@
 #!/bin/bash
 
-echo "Baixando vim e suas dependências"
-sudo apt-get install vim-nox cmake python-dev
+echo "Instalando dependências"
+sudo apt-get install build-essential cmake git python3-dev -y
+
+echo "Instalando o vim"
+sudo apt-get install vim-nox -y
 
 echo "Clonando repositório do esquema de cores"
 git clone https://gist.github.com/8248094.git .vim/colors/monokai
+
 echo "Movendo esquema de cores para a pasta colors [diretorio clone]"
 mv .vim/colors/monokai/Monokai.vim .vim/colors/monokai.vim
+
 echo "Deletando pasta do repositório clonado"
 rm -rf .vim/colors/monokai
 
 echo "Copiando diretório clonado para a pasta do vim"
 cp -r .vim ~/
+
 echo "Copiando vimrc para a pasta do vim"
 cp .vimrc ~/
 
+echo "Clonando Vundle"
 git clone https://github.com/VundleVim/Vundle.vim.git ~/.vim/bundle/Vundle.vim
 
+echo "Instalando plugins"
 vim +PluginInstall +qall
 
-python ~/.vim/bundle/YouCompleteMe/install.py
-
+echo "Instalando plugins"
+python3 ~/.vim/bundle/YouCompleteMe/install.py
