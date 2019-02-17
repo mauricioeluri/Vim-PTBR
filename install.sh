50f965b76d7aa32f9baab6cbef45c133a6a1e417
Maurício El Uri <mauriciom.eluri@gmail.com>
2019-02-17 13:15:01 -0300

    Ordem do script ajustada

--- install.sh
+++ install.sh
@@ -1,13 +1,19 @@
 #!/bin/bash
 
+echo "Baixando vim e suas dependências"
 sudo apt-get install vim-nox cmake python-dev
 
-cp -r .vim ~/
-cp .vimrc ~/
-
+echo "Clonando repositório do esquema de cores"
 git clone https://gist.github.com/8248094.git .vim/colors/monokai
+echo "Movendo esquema de cores para a pasta colors [diretorio clone]"
 mv .vim/colors/monokai/Monokai.vim .vim/colors/monokai.vim
+echo "Deletando pasta do repositório clonado"
 rm -rf .vim/colors/monokai
+
+echo "Copiando diretório clonado para a pasta do vim"
+cp -r .vim ~/
+echo "Copiando vimrc para a pasta do vim"
+cp .vimrc ~/
 
 git clone https://github.com/VundleVim/Vundle.vim.git ~/.vim/bundle/Vundle.vim
 
