933bd214e25c2ad7d7abfdc0b6d586730108b645
Maurício El Uri <22596610+mauricioeluri@users.noreply.github.com>
2020-01-23 19:10:05 -0300

    Script melhorado com submodules

--- install.sh
+++ install.sh
@@ -6,23 +6,20 @@ sudo apt-get install build-essential cmake git python3-dev -y
 echo "Instalando o vim"
 sudo apt-get install vim-nox -y
 
-echo "Clonando repositório do esquema de cores"
-git clone https://gist.github.com/8248094.git .vim/colors/monokai
+echo "Criando pasta colors"
+mkdir -p ~/.vim/colors
 
-echo "Movendo esquema de cores para a pasta colors [diretorio clone]"
-mv .vim/colors/monokai/Monokai.vim .vim/colors/monokai.vim
-
-echo "Deletando pasta do repositório clonado"
-rm -rf .vim/colors/monokai
-
-echo "Copiando diretório clonado para a pasta do vim"
-cp -r .vim ~/
+echo "Copiando esquema de cores para a pasta colors"
+cp monokai/Monokai.vim ~/.vim/colors/monokai.vim
 
 echo "Copiando vimrc para a pasta do vim"
 cp .vimrc ~/
 
-echo "Clonando Vundle"
-git clone https://github.com/VundleVim/Vundle.vim.git ~/.vim/bundle/Vundle.vim
+echo "Criando pasta bundle"
+mkdir -p ~/.vim/bundle
+
+echo "Copiando Vundle"
+cp vundle ~/.vim/bundle/Vundle.vim
 
 echo "Instalando plugins"
 vim +PluginInstall +qall
